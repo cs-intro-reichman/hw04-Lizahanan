1b9c6f333bdc0325b7e6c57621e9d16ea17fb649
Your Name <you@example.com>
2024-01-12 02:04:39 +0300

    findmissing commit

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,10 +1,29 @@
 public class ArrayOps {
     public static void main(String[] args) {
         
+    
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
+        int n = array.length;
+        for(int i = 0; i<=n; i++)
+        {
+            boolean isPresentFlag = false;
+            for(int j = 0; j < array.length; j ++)
+            {
+
+                if (array[j] == i)
+                {
+                    isPresentFlag = true;
+                }
+
+            }
+        if(!isPresentFlag)
+        {
+            return i;
+        }
+
+        }
         return -1;
     }
 
